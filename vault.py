e19aa0136cdc9809cb52020e53bfa16f90d728e9
Kamil Kapłon <122369882+Cherrox93@users.noreply.github.com>
2026-03-05 16:30:07 +0100

    Remove sensitive data from TelegramConfig
    Removed sensitive Telegram token and chat_id values.

--- vault.py
+++ vault.py
@@ -3,18 +3,18 @@
 from config import TelegramConfig, ExchangeConfig
 
 TELEGRAM_SCALPER = TelegramConfig(
-    token="8352544907:AAFCVw7t2mGpMPZ2Ik-TVmGAveniuL08kk4",
-    chat_id="1709174569"
+    token="",
+    chat_id=""
 )
 
 TELEGRAM_LOWCAP = TelegramConfig(
-    token="8638878426:AAFvkClWyhATahO8KSjTlNnHOXcspqpA9bY",
-    chat_id="1709174569"
+    token="",
+    chat_id=""
 )
 
 TELEGRAM_PUMP = TelegramConfig(
-    token="8782933984:AAGQdAFWHuCvsnOXKvTOVf53J9TUvUmWZsQ",
-    chat_id="1709174569"
+    token="",
+    chat_id=""
 )
 
 EXCHANGE = ExchangeConfig(
